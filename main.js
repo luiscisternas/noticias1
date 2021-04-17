eead7812adb5ca4736704120101004bd59e14061
luiscisternas <81481919+luiscisternas@users.noreply.github.com>
2021-04-17 12:25:45 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const elementosCarousel = document.querySelectorAll('.carousel');
+    M.Carousel.init(elementosCarousel, {
+        duration: 150,
+        dist:-150,
+        shift:5,
+        padding:5,
+        numVisible:3,
+        indicators: true,
+        
+    })
+});
+
+
+
+var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
+	overlay = document.getElementById('overlay'),
+	popup = document.getElementById('popup'),
+	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+
+btnAbrirPopup.addEventListener('click', function(){
+	overlay.classList.add('active');
+	popup.classList.add('active');
+});
+
+btnCerrarPopup.addEventListener('click', function(e){
+	e.preventDefault();
+	overlay.classList.remove('active');
+	popup.classList.remove('active');
+});
+
+
+
+
+
